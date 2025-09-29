210f4b67c3a1059a202baf487932d412995d38e0
mc-Tongtianyema <3824181006@qq.com>
2025-09-29 21:45:50 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 input.onButtonPressed(Button.A, function () {
-	
+    reading = input.lightLevel()
+    led.plotBarGraph(
+    reading,
+    225
+    )
+    basic.showNumber(reading)
 })
+let reading = 0
 basic.showString("Hello!")
 basic.showString("This is Tools")
 basic.showLeds(`
