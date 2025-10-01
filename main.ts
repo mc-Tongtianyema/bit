2e43825f99da93187d55bb3f9fe705d2634eafc9
mc-Tongtianyema <3824181006@qq.com>
2025-10-01 17:04:08 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,21 @@
+let start = 0
+let elapsed = 0
 let reading = 0
+let degrees = 0
 let X = 0
 let y = 0
+// 计时器开始，不用多说了吧
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    start = 0
+    while ((0 as any) < (1 as any)) {
+        basic.pause(1000)
+        elapsed += 1
+    }
+})
+// 计时器输出，能多次按，想要多次按的话就多按几次，都是取的第一次开始到现在的秒数
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    basic.showNumber(elapsed)
+})
 // 这里是测试亮度的
 input.onButtonPressed(Button.A, function () {
     reading = input.lightLevel()
@@ -9,6 +24,19 @@ input.onButtonPressed(Button.A, function () {
     225
     )
     basic.showNumber(reading)
+})
+// 不是指南针还能是啥？
+input.onButtonPressed(Button.AB, function () {
+    degrees = input.compassHeading()
+    if (degrees < 45) {
+        basic.showString("N")
+    } else if (degrees < 135) {
+        basic.showString("E")
+    } else if (degrees < 225) {
+        basic.showString("S")
+    } else if (degrees < 315) {
+        basic.showString("W")
+    }
 })
 // 这里的东西尽量不要乱动，我怕你不会
 input.onButtonPressed(Button.B, function () {
@@ -22,6 +50,7 @@ input.onButtonPressed(Button.B, function () {
         basic.showIcon(IconNames.Happy)
     }
 })
+// 字面意思，是个小白都知道吧
 input.onSound(DetectedSound.Loud, function () {
     basic.clearScreen()
 })
