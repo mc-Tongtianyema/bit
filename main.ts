4a67b24d558620cee37d3901c4c5365756eb0ae3
mc-Tongtianyema <3824181006@qq.com>
2025-10-01 15:40:23 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,4 @@
 let reading = 0
-input.onPinPressed(TouchPin.P0, function () {
-    basic.clearScreen()
-})
 input.onButtonPressed(Button.A, function () {
     reading = input.lightLevel()
     led.plotBarGraph(
