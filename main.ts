e1a3f26f57b0946af03b4eddd7b5b3e00be7f583
mc-Tongtianyema <3824181006@qq.com>
2025-10-01 16:25:15 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 let reading = 0
+let X = 0
+let y = 0
 input.onButtonPressed(Button.A, function () {
     reading = input.lightLevel()
     led.plotBarGraph(
@@ -8,5 +10,13 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(reading)
 })
 input.onButtonPressed(Button.B, function () {
-	
+    X = input.acceleration(Dimension.X)
+    y = input.acceleration(Dimension.Y)
+    if (Math.abs(X) > 32) {
+        basic.showIcon(IconNames.Sad)
+    } else if (Math.abs(y) > 32) {
+        basic.showIcon(IconNames.Angry)
+    } else {
+        basic.showIcon(IconNames.Happy)
+    }
 })
